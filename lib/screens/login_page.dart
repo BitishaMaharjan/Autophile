3ba7e3482191054049934e0099fca162aa9aa9b5
Aaniya Thapa <aaniyathapa22@gmail.com>
2024-11-25 17:38:30 +0545

    added social icons

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -112,7 +112,91 @@ class _Login_PageState extends State<Login_Page> {
                 )),
               ),
               SizedBox(height: 20,),
-              AppButton(text: "Sign in", onTap: (){})
+              AppButton(text: "Sign in", onTap: (){}),
+              SizedBox(height: 30,),
+              Row(
+                children: [
+                  Expanded(child: Divider(color:Colors.grey),
+                  ),
+                  Padding(padding: const EdgeInsets.symmetric(horizontal: 50),
+                  child: Text("Or",
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.grey
+                  ),),),
+                  Expanded(child: Divider(color: Colors.grey,))
+                ],
+              ),
+              SizedBox(height: 30,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    onTap: (){
+
+                    },
+                    child: Container(
+                      width: 120,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        border: Border.all(color:Color(0xffD8DADC)),
+                        borderRadius: BorderRadius.circular(10),
+
+                      ),
+                      padding: EdgeInsets.all(15),
+                      child: Image.asset(
+                        'assets/Social icons/Facebook.png',
+                        height: 20,
+                        width: 20,
+
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 20,),
+                  GestureDetector(
+                    onTap: (){
+
+                    },
+                    child: Container(
+                      width: 120,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        border: Border.all(color: Color(0xffD8DADC)),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      padding: EdgeInsets.all(15),
+                      child: Image.asset(
+                        'assets/Social icons/Googlelogo.png',
+                        height: 20,
+                        width: 20,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(height: 150,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Don't have an account?",
+                  style: TextStyle(
+                      fontSize:17,
+                  color: Colors.black,
+                    fontWeight: FontWeight.w400,),
+                  ),
+                  GestureDetector(
+                    onTap: (){
+
+                    },
+                    child: Text("Sign up",
+                    style: TextStyle(
+                      fontSize: 17,
+                      color:Colors.blueGrey,
+                      fontWeight: FontWeight.w400,
+                    ),),
+                  )
+                ],
+              )
             ],
           ),
         ),
