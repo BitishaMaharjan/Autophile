b22b1eec1349a1cc2a4ac9fbbe2b56bcfe11729a
Bitisha Maharjan <bitisha.maharjan@bajratechnologies.com>
2024-11-26 21:57:37 +0545

    feat: add routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,38 @@
-import 'package:autophile/screens/SplashWrapper.dart';
-import 'package:autophile/screens/landing_page.dart';
-import 'package:autophile/screens/settings_page.dart';
-import 'package:autophile/themes/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:autophile/screens/MainScreen.dart';
+import 'package:autophile/themes/theme_provider.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-      runApp(
-      ChangeNotifierProvider(
+  runApp(
+    ChangeNotifierProvider(
       create: (context) => ThemeProvider(),
-  child: const MyApp(),
-  ),
+      child: const MyApp(),
+    ),
   );
-}
-void _onCameraTapped() {
-  print("Camera tapped!");
-  // Add camera logic here
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  void _onCameraTapped() {
+    print('Camera tapped');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Autophile',
-      theme: Provider.of<ThemeProvider>(context).themeData,
-      home: const SplashWrapper(),
-      debugShowCheckedModeBanner: false,
-      bottomNavigationBar: CustomBottomNavBar(
-        currentIndex: _selectedIndex,
-        onItemTapped: _onItemTapped,
-        onCameraTapped: _onCameraTapped,
-      ),
+    return Consumer<ThemeProvider>(
+      builder: (context, themeProvider, child) {
+        return MaterialApp(
+          title: 'Autophile',
+          theme: themeProvider.themeData,
+          debugShowCheckedModeBanner: false,
+          initialRoute: '/home',
+          routes: {
+            '/home': (context) => BaseScreen(onCameraTapped: _onCameraTapped),
+          },
+        );
+      },
     );
   }
 }
