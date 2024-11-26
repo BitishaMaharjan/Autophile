1581dcf1a48d14c7a9e0f27c72a2e05430f16e5a
Bitisha Maharjan <bitisha.maharjan@bajratechnologies.com>
2024-11-26 21:56:49 +0545

    feat: make a lsyout for main screens

---
+++ lib/screens/MainScreen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:autophile/widgets/BottomNavBar.dart';
+
+class BaseScreen extends StatefulWidget {
+  final VoidCallback onCameraTapped;
+
+  const BaseScreen({Key? key, required this.onCameraTapped}) : super(key: key);
+
+  @override
+  _BaseScreenState createState() => _BaseScreenState();
+}
+
+class _BaseScreenState extends State<BaseScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = [
+    Center(
+      child: Text(
+        "Home Screen",
+        style: TextStyle(fontSize: 24, color: Colors.black),
+      ),
+    ),
+    Center(
+      child: Text(
+        "Search Screen",
+        style: TextStyle(fontSize: 24, color: Colors.black),
+      ),
+    ),
+    Center(
+      child: Text(
+        "Notification Screen",
+        style: TextStyle(fontSize: 24, color: Colors.black),
+      ),
+    ),
+    Center(
+      child: Text(
+        "Settings Screen",
+        style: TextStyle(fontSize: 24, color: Colors.black),
+      ),
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _screens,
+      ),
+      bottomNavigationBar: CustomBottomNavBar(
+        currentIndex: _selectedIndex,
+        onItemTapped: _onItemTapped,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: widget.onCameraTapped,
+        backgroundColor: Colors.lightBlue,
+        shape: const CircleBorder(), // Ensures the FAB is completely circular
+        child: const Icon(Icons.camera_alt, color: Colors.white),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked, // Keeps FAB centered
+    );
+  }
+
+}
