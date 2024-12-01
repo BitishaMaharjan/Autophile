898a848b85e62f59ceaf4a9006f76497183c7a61
Bitisha Maharjan <bitisha.maharjan@bajratechnologies.com>
2024-12-01 08:40:37 +0545

    feat: add notification path in nav

--- lib/screens/base_screen.dart
+++ lib/screens/base_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:autophile/widgets/custom_bottom_navbar.dart';
+import 'package:autophile/screens/Notification.dart';
+import 'package:autophile/themes/light_mode.dart';
 
 class BaseScreen extends StatefulWidget {
   final VoidCallback onCameraTapped;
@@ -15,7 +17,7 @@ class _BaseScreenState extends State<BaseScreen> {
 
   final List<Widget> _screens = [
     Center(
-      child: Text(
+      child:Text(
         "Home Screen",
         style: TextStyle(fontSize: 24, color: Colors.black),
       ),
@@ -27,10 +29,7 @@ class _BaseScreenState extends State<BaseScreen> {
       ),
     ),
     Center(
-      child: Text(
-        "Notification Screen",
-        style: TextStyle(fontSize: 24, color: Colors.black),
-      ),
+      child: NotificationPage(),
     ),
     Center(
       child: Text(
@@ -49,9 +48,16 @@ class _BaseScreenState extends State<BaseScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: IndexedStack(
-        index: _selectedIndex,
-        children: _screens,
+      backgroundColor:  Theme.of(context).colorScheme.surface, // Set the background color here
+      body: SafeArea(
+        // Ensures content starts below the status bar
+        child: Padding(
+          padding: const EdgeInsets.only(top: 5.0), // Adds extra spacing at the top
+          child: IndexedStack(
+            index: _selectedIndex,
+            children: _screens,
+          ),
+        ),
       ),
       bottomNavigationBar: CustomBottomNavBar(
         currentIndex: _selectedIndex,
@@ -66,5 +72,4 @@ class _BaseScreenState extends State<BaseScreen> {
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked, // Keeps FAB centered
     );
   }
-
 }
