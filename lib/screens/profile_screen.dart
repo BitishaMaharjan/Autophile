912b81a622f0dcf81021659c44c7ff75edc511d6
Aaniya Thapa <aaniyathapa22@gmail.com>
2024-12-04 09:46:14 +0545

    profileheader added to the profile screen with the components till my posts and saved photos

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,3 +1,4 @@
+import 'package:autophile/widgets/profile_header.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -8,21 +9,109 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  bool showMyPosts=true;
   @override
   Widget build(BuildContext context) {
-    final screenWidth=MediaQuery.of(context).size.width;
-    final screenHeight=MediaQuery.of(context).size.height;
-    return Scaffold(
-      body:SafeArea(
-          child:Container(
-            color: Theme.of(context).colorScheme.surface,
-            child: SingleChildScrollView(
-              child: Padding(padding: EdgeInsets.symmetric(
-                
-              )),
-            ),
-          ) ,)
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
 
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.surface,
+          ),
+          child: Stack(
+            children: [
+              // Main Content
+              SingleChildScrollView(
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    top:21,
+                    right: 74,
+                    left: 74
+
+                  ),
+                  child: Column(
+                    children: [
+                      ProfileHeader(
+                        profileImageUrl: "assets/images/profile _picture.png",
+                        name: "Bitisha Maharjan",
+                        location: "Kathmandu, Nepal",
+                        bio: "Car enthusiast / heritage lover",
+                        onEditProfile: () {},
+                      ),
+                      const SizedBox(height: 26,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          GestureDetector(
+                            onTap: (){
+                              setState(() {
+                                showMyPosts=true;
+                              });
+                            },
+                            child: Text(
+                              "My Posts",
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w600,
+                                  color: showMyPosts ?Theme.of(context).colorScheme.secondary
+                                :Theme.of(context).colorScheme.onPrimary,
+                              ),
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: (){
+                              setState(() {
+                                showMyPosts=false;
+                              });
+                            },
+                            child: Text(
+                              "Saved Photos",
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w600,
+                                color: Theme.of(context).colorScheme.secondary,
+                              ),
+
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 13), // Gap below the section
+                      Divider(
+                        color: Colors.grey.shade400,
+                        thickness: 1,
+                      ),
+                      const SizedBox(height: 14,),
+
+                    ],
+                  ),
+                ),
+              ),
+
+              // Settings Icon Positioned
+              Positioned(
+                top: screenHeight * 0.03,
+                right: screenWidth * 0.05,
+                child: GestureDetector(
+                  onTap: () {
+                  },
+                  child: const Icon(
+                    Icons.settings,
+                    size: 32,
+
+                  ),
+                ),
+              ),
+
+
+            ],
+
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/widgets/profile_header.dart
+++ lib/widgets/profile_header.dart
@@ -19,21 +19,22 @@ class ProfileHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         CircleAvatar(
-          radius:50 ,
+          radius: 80,
           backgroundImage: NetworkImage(profileImageUrl),
         ),
-        const SizedBox(height: 7,),
+        const SizedBox(height: 7),
         Text(
           name,
           style: const TextStyle(
             fontSize: 18,
             fontWeight: FontWeight.w600,
-
           ),
         ),
-        const SizedBox(height: 14,),
+        const SizedBox(height: 14),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -42,52 +43,46 @@ class ProfileHeader extends StatelessWidget {
               size: 18,
               color: Colors.grey,
             ),
-            const SizedBox(width: 4.75,),
+            const SizedBox(width: 4.75),
             Text(
               location,
               style: const TextStyle(
                 fontSize: 13,
                 fontWeight: FontWeight.w500,
-                color: Colors.grey
+                color: Colors.grey,
               ),
             ),
-            const SizedBox(height: 14,),
-            Text(
-              bio,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w400,
-                color: Colors.black,
-              ),
-            ),
-            const SizedBox(height: 16,),
-            ElevatedButton(onPressed: onEditProfile,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xff2E3448),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(11),
-                  ),
-
-                  padding:const EdgeInsets.symmetric(horizontal: 15,vertical: 12),
-
-                ),
-                child: const Text(
-                  "Edit Profile",
-                  style: TextStyle(
-
-                    fontSize: 15,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ))
           ],
-        )
+        ),
+        const SizedBox(height: 14),
+        Text(
+          bio,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+            color: Colors.black,
+          ),
+        ),
+        const SizedBox(height: 16),
+        ElevatedButton(
+          onPressed: onEditProfile,
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color(0xff2E3448),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(11),
+            ),
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 12),
+          ),
+          child: const Text(
+            "Edit Profile",
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ),
       ],
-
     );
   }
 }
-
-
-
-
