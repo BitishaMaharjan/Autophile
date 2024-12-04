d495ec7755720356aa46fd49d5ca33362a9dedb2
Aaniya Thapa <aaniyathapa22@gmail.com>
2024-12-05 00:14:37 +0545

    added app drawer tile in app drawer widget

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -2,6 +2,7 @@ import 'package:autophile/widgets/home_screen/post_list_widget.dart';
 import 'package:autophile/widgets/profile_header.dart';
 import 'package:flutter/material.dart';
 
+import '../widgets/app_drawer.dart';
 import '../widgets/saved_photos.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -12,7 +13,10 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  bool showMyPosts=true;
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  bool showMyPosts = true;
+
   final List<Map<String, String>> posts = [
     {
       'user': 'Car Guy',
@@ -35,147 +39,130 @@ class _ProfileScreenState extends State<ProfileScreen> {
       'shares': '60',
     },
   ];
+
   final List<Map<String, String>> savedPhotos = [
     {
-      'image':
-      'assets/images/savedcar1.png',
+      'image': 'assets/images/savedcar1.png',
       'name': 'Volkswagen Polo',
     },
     {
-      'image':
-      'assets/images/savedcar2.png',
+      'image': 'assets/images/savedcar2.png',
       'name': 'Tesla Model X',
     },
     {
-      'image':
-      'assets/images/savedcar3.png',
+      'image': 'assets/images/savedcar3.png',
       'name': 'Mercedes G-Wagon',
     },
     {
-      'image':
-      'assets/images/savedcar1.png',
+      'image': 'assets/images/savedcar1.png',
       'name': 'BMW X5',
     },
     {
-      'image':
-      'assets/images/savedcar2.png',
+      'image': 'assets/images/savedcar2.png',
       'name': 'BMW X5',
     },
     {
-      'image':
-      'assets/images/savedcar3.png',
+      'image': 'assets/images/savedcar3.png',
       'name': 'BMW X5',
     },
-
   ];
-
 
   @override
   Widget build(BuildContext context) {
-    final screenWidth = MediaQuery.of(context).size.width;
-    final screenHeight = MediaQuery.of(context).size.height;
-
     return Scaffold(
+      key: _scaffoldKey, // Assign the GlobalKey to the Scaffold
+      endDrawer: const AppDrawer(),
       body: SafeArea(
         child: Container(
           decoration: BoxDecoration(
             color: Theme.of(context).colorScheme.surface,
           ),
-          child: Stack(
-            children: [
-              // Main Content
-              SingleChildScrollView(
-                child: Padding(
-                  padding: EdgeInsets.only(
-                    top:21,
-                    right: 20,
-                    left: 20,
-
-                  ),
-                  child: Column(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
+                children: [
+                  Stack(
                     children: [
-                      ProfileHeader(
-                        profileImageUrl: "assets/images/profile _picture.png",
-                        name: "Bitisha Maharjan",
-                        location: "Kathmandu, Nepal",
-                        bio: "Car enthusiast / heritage lover",
-                        onEditProfile: () {},
-                      ),
-                      const SizedBox(height: 26,),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      Column(
                         children: [
-                          GestureDetector(
-                            onTap: (){
-                              setState(() {
-                                showMyPosts=true;
-                              });
-                            },
-                            child: Text(
-                              "My Posts",
-                              style: TextStyle(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w600,
-                                  color: showMyPosts ?Theme.of(context).colorScheme.secondary
-                                :Theme.of(context).colorScheme.onPrimary,
-                              ),
-                            ),
+                          const SizedBox(height: 10),
+                          ProfileHeader(
+                            profileImageUrl: "assets/images/profile _picture.png",
+                            name: "Bitisha Maharjan",
+                            location: "Kathmandu, Nepal",
+                            bio: "Car enthusiast / heritage lover",
+                            onEditProfile: () {},
                           ),
-                          GestureDetector(
-                            onTap: (){
-                              setState(() {
-                                showMyPosts=false;
-                              });
-                            },
-                            child: Text(
-                              "Saved Photos",
-                              style: TextStyle(
-                                fontSize: 14,
-                                fontWeight: FontWeight.w600,
-                                color: !showMyPosts
-                                    ? Theme.of(context).colorScheme.secondary
-                                    : Theme.of(context).colorScheme.onPrimary, // Inactive color
+                          const SizedBox(height: 26),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    showMyPosts = true;
+                                  });
+                                },
+                                child: Text(
+                                  "My Posts",
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w600,
+                                    color: showMyPosts
+                                        ? Theme.of(context).colorScheme.secondary
+                                        : Theme.of(context).colorScheme.onPrimary,
+                                  ),
+                                ),
                               ),
-
-                            ),
+                              GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    showMyPosts = false;
+                                  });
+                                },
+                                child: Text(
+                                  "Saved Photos",
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w600,
+                                    color: !showMyPosts
+                                        ? Theme.of(context).colorScheme.secondary
+                                        : Theme.of(context).colorScheme.onPrimary,
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
+                          const SizedBox(height: 13), // Gap below the section
+                          Divider(
+                            color: Colors.grey.shade400,
+                            thickness: 1,
+                          ),
+                          const SizedBox(height: 14),
+                          showMyPosts
+                              ? PostListWidget(posts: posts) // My Posts
+                              : SavedPhotosWidget(savedPhotos: savedPhotos),
                         ],
                       ),
-                      const SizedBox(height: 13), // Gap below the section
-                      Divider(
-                        color: Colors.grey.shade400,
-                        thickness: 1,
+                      Positioned(
+                        top: 11,
+                        right: 0,
+                        child: GestureDetector(
+                          onTap: () {
+                            _scaffoldKey.currentState!.openEndDrawer(); // Use the GlobalKey to open the drawer
+                          },
+                          child: const Icon(
+                            Icons.settings,
+                            size: 34,
+                          ),
+                        ),
                       ),
-                      const SizedBox(height: 14,),
-                      showMyPosts
-                          ? PostListWidget(posts: posts) // My Posts
-                          : SavedPhotosWidget(savedPhotos: savedPhotos),
-
-
-
                     ],
                   ),
-                ),
+                ],
               ),
-
-              // Settings Icon Positioned
-              Positioned(
-                top: screenHeight * 0.03,
-                right: screenWidth * 0.05,
-                child: GestureDetector(
-                  onTap: () {
-                  },
-                  child: const Icon(
-                    Icons.settings,
-                    size: 32,
-
-                  ),
-                ),
-              ),
-
-
-            ],
-
+            ),
           ),
         ),
       ),
--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -10,25 +10,99 @@ class AppDrawer extends StatelessWidget {
     return Drawer(
       backgroundColor: Theme.of(context).colorScheme.surface,
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Padding(
-            padding: const EdgeInsets.only(top:100),
-            child: Icon(Icons.lock_open_rounded,size: 80,color: Theme.of(context).colorScheme.inversePrimary,),
+            padding: const EdgeInsets.only(
+                top:92,
+              left: 36,
+              right: 11
+
+            ),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  backgroundImage: AssetImage('assets/images/profile _picture.png'),
+
+                ),
+                const SizedBox(width: 12,),
+                Column(
+                  children: [
+                    Text("Bitisha Maharjan",
+                    style: TextStyle(
+                      fontSize:15,
+                        fontWeight: FontWeight.w600,
+
+                    ),),
+                    const SizedBox( height: 3,),
+                    Text("bits2005@gmail.com",
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w500,
+                        color: Theme.of(context).colorScheme.onPrimary,
+                    ),)
+                  ],
+                )
+
+              ],
+
+            ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: Divider(),
+          SizedBox(height: 80,),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center, // Center all tiles
+              children: [
+                AppDrawerTile(
+                  text: 'Language',
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icons.language,
+                ),
+                AppDrawerTile(
+                  text: 'Help & Support',
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const SettingsPage()),
+                    );
+                  },
+                  icon: Icons.question_mark_rounded,
+                ),
+                AppDrawerTile(
+                  text: 'Terms and Policies',
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icons.policy,
+                ),
+                AppDrawerTile(
+                  text: 'Report a Problem',
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icons.report_problem_rounded,
+                ),
+                AppDrawerTile(
+                  text: 'Change Password',
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icons.lock_reset_rounded,
+                ),
+                AppDrawerTile(
+                  text: 'Log Out',
+                  onTap: () {},
+                  icon: Icons.logout,
+                ),
+              ],
+            ),
           ),
-          AppDrawerTile(text: 'H O M E', onTap: (){
-            Navigator.pop(context);
-          }, icon: Icons.home),
-          AppDrawerTile(text: 'S E T T I N G S', onTap: (){
-            Navigator.pop(context);
-            Navigator.push(context, MaterialPageRoute(builder: (context)=>const SettingsPage()));
-          }, icon: Icons.settings),
-          const Spacer(),
-          AppDrawerTile(text: 'L O G O U T', onTap: (){}, icon: Icons.logout),
-          const SizedBox(height: 25,)
+
+
         ],
       ),
     );
