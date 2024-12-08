0e023e3245ea72bcf8fb480a7c4b85c3d4d30e52
Bijen Shrestha <shrbijen31@gmail.com>
2024-12-08 15:02:39 +0545

    theme management in the storage

--- lib/themes/theme_provider.dart
+++ lib/themes/theme_provider.dart
@@ -1,24 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:autophile/themes/dark_mode.dart';
 import 'package:autophile/themes/light_mode.dart';
-import 'package:flutter/material.dart';
 
-class ThemeProvider with ChangeNotifier{
+class ThemeProvider with ChangeNotifier {
   ThemeData _themeData = lightMode;
+  final FlutterSecureStorage _secureStorage = FlutterSecureStorage();
 
   ThemeData get themeData => _themeData;
 
   bool get isDarkMode => _themeData == darkMode;
 
-  set themeData(ThemeData themeData){
+  set themeData(ThemeData themeData) {
     _themeData = themeData;
+    _saveThemeToStorage(themeData == darkMode ? 'dark' : 'light');
     notifyListeners();
   }
 
-  void toggleTheme(){
-    if(_themeData == lightMode){
+  void toggleTheme() {
+    if (_themeData == lightMode) {
       themeData = darkMode;
-    }else{
+    } else {
       themeData = lightMode;
     }
+  }
+
+  Future<void> loadTheme() async {
+    String? storedTheme = await _secureStorage.read(key: 'theme');
+    print("Loaded theme: $storedTheme");
+
+    if (storedTheme == 'dark') {
+      themeData = darkMode;
+    } else {
+      themeData = lightMode;
+    }
+    notifyListeners();
+  }
+
+  Future<void> _saveThemeToStorage(String theme) async {
+    await _secureStorage.write(key: 'theme', value: theme);
   }
 }
