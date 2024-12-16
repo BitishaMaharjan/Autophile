8874cca73202ec8b19648651c1f407c963b87da7
Bitisha Maharjan <bitisha.maharjan@bajratechnologies.com>
2024-12-16 08:09:11 +0545

    feat: add saved photo to firebase

--- lib/services/image_modal_helper.dart
+++ lib/services/image_modal_helper.dart
@@ -2,21 +2,34 @@ import 'dart:io';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:image/image.dart' as img; // Import image package
 
 class ImageModalService {
-  /// Saves the image, label, and confidence to Firebase Firestore.
+  final FlutterSecureStorage storage = FlutterSecureStorage();  // Initialize the storage
+
+  /// Saves the image, label, confidence, and userId to Firebase Firestore.
   Future<void> saveToFirebase(String label, String confidence, File imageFile) async {
     try {
       print(label);
-      // Convert image to Base64
-      final imageBytes = await imageFile.readAsBytes();
-      final base64Image = 'hbhbnnu';
+
+      // Fetch the userId from storage
+      final userId = await storage.read(key: 'userId');
+
+      // Compress the image
+      final img.Image image = img.decodeImage(imageFile.readAsBytesSync())!;
+      final img.Image resizedImage = img.copyResize(image, width: 800);  // Resize the image to 800px width (adjust as needed)
+
+      // Convert compressed image to bytes and then to Base64
+      final List<int> compressedBytes = img.encodeJpg(resizedImage, quality: 85);  // Compress and set quality (0-100)
+      final base64Image = base64Encode(compressedBytes);  // Convert to Base64
 
       // Save to Firestore
       await FirebaseFirestore.instance.collection('classified_images').add({
         'label': label,
         'confidence': confidence,
         'image': base64Image,
+        'userId': userId,  // Store the userId
         'timestamp': DateTime.now().toIso8601String(),
       });
 
@@ -28,6 +41,8 @@ class ImageModalService {
 
   /// Shows a modal dialog asking the user if they want to save the image.
   void showSaveModal(BuildContext context, String label, String confidence, File imageFile) {
+    final theme = Theme.of(context);  // Access current theme
+
     showDialog(
       context: context,
       builder: (ctx) => AlertDialog(
@@ -39,6 +54,8 @@ class ImageModalService {
               Navigator.of(ctx).pop(); // Close the dialog
             },
             child: const Text('Cancel'),
+
+
           ),
           TextButton(
             onPressed: () {
@@ -46,6 +63,7 @@ class ImageModalService {
               saveToFirebase(label, confidence, imageFile);
             },
             child: const Text('Save'),
+
           ),
         ],
       ),
